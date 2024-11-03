6e4f3794ace871977ce22a72ebad892169508863
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-11-03 23:40:24 +0600

    Arrow Function in JavaScript

---
+++ arrow function.js
@@ -0,0 +1,16 @@
+// Arrow Function in JavaScript
+
+// function add (a, b){
+//     return a + b
+// }
+
+
+
+let add = (a, b)=> a + b
+
+
+console.log(add(7, 6));
+
+
+let sqr = x => x*x
+console.log(sqr(4));
