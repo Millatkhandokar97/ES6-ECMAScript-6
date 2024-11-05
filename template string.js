29d5655a6325fb82bbf6aa9b0be43afd2a5427e9
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-11-05 16:43:52 +0600

    Template String in javascript

---
+++ template string.js
@@ -0,0 +1,13 @@
+// Template String in javascript
+
+let templateString = `Template String 
+    in 
+        javascript`
+console.log(templateString);
+
+
+let name = 'MD Millat'
+let age = 24
+
+console.log('My name is ' + name + ' and I am ' + age + ' years old');
+console.log(`My name is ${name} and I am ${age} years old. I am ${age < 18 ? 'not':''}adult`);
