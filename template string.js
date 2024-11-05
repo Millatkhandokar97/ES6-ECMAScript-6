bfb6c408222cf4c146cf8b320c3c01fa727c5724
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-11-05 16:53:26 +0600

    Update template string.js

--- template string.js
+++ template string.js
@@ -10,4 +10,4 @@ let name = 'MD Millat'
 let age = 24
 
 console.log('My name is ' + name + ' and I am ' + age + ' years old');
-console.log(`My name is ${name} and I am ${age} years old. I am ${age < 18 ? 'not':''}adult`);
+console.log(`My name is ${name} and I am ${age} years old. I am ${age < 18 ? 'not ':''}adult`);
