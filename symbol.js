a3cd7d5e7b04cc4302b121356fb87e8664a4bef6
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-11-08 23:26:23 +0600

    symbol in javascript

---
+++ symbol.js
@@ -0,0 +1,17 @@
+//symbol in javascript
+
+let s1 = Symbol()
+let s2 = Symbol('test symble')
+
+console.log(s1);
+console.log(s2);
+
+console.log(s1 === s2);
+
+
+let toss ={
+    Head: Symbol('Head'),
+    Tail: Symbol('Tail')
+}
+
+
