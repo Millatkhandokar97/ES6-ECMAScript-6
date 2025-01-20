dd2891fa2f4ba383d8cac1cb0cb1e4e587931e27
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-01-20 21:38:49 +0600

    Template String in Javascript

---
+++ Template String in Javascript.js
@@ -0,0 +1,12 @@
+// Template String in Javascript
+
+let templateString = `this is Millat, 
+
+web development is passion
+
+
+        also programing language 
+                JavaScript `
+
+
+console.log(templateString);
