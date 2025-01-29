d137335cccc1e751da20dc74c5f760c0cbf3d14e
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-01-29 23:56:18 +0600

    Static Method in Javascript

---
+++ Static Method in Javascript.js
@@ -0,0 +1,27 @@
+// Static Method in Javascript
+
+class Person {
+    constructor(name, email){
+        this.name = name
+        this.email = email
+    }
+
+    print() {
+        console.log(this.name, this.email);
+    }
+    static create(str){
+        let person = JSON.parse(str)
+        return new Person(person.name, person.email)
+
+    }
+}
+
+let str = '{"name": "MD Millat", "email": "millatkhandokar@gmail.com"}'
+
+let p1 = Person.create(str)
+
+console.log(p1);
+
+console.log(p1 instanceof Person);
+
+p1.print()
