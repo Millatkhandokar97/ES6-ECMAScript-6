3b12317cc2f23737a332b2328a7b202e3d40110b
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-02-04 03:22:46 +0600

    This in Javascript Class

--- This in Javascript Class.js
+++ This in Javascript Class.js
@@ -1,1 +1,49 @@
 // "This" in Javascript Class
+
+'use strict'
+
+
+class Person {
+    constructor(name, email){
+        this.name = name
+        this.email = email
+    }
+
+    print() {
+        console.log(this.name, this.email);
+    }
+
+    test(){
+        console.log(this);
+    }
+    static create(str){
+        let person = JSON.parse(str)
+        return new Person(person.name, person.email)
+
+    }
+}
+
+let str = '{"name": "MD Millat", "email": "millatkhandokar@gmail.com"}'
+
+let p1 = Person.create(str)
+
+console.log(p1);
+
+console.log(p1 instanceof Person);
+
+p1.print()
+
+
+
+// function Shape(){
+//     this .draw = function(){
+//         console.log(this);
+//     }
+// }
+
+// let s1 = new Shape()
+// let anotherDraw = s1.draw
+// anotherDraw()
+
+let test = p1.test
+test()
