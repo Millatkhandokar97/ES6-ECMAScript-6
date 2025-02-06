a169904476f94bdd6665323d3ef63da127dc0905
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-02-06 19:38:48 +0600

    Private Properties with WeakMap in Javascript

---
+++ Private Properties with WeakMap in Javascript.js
@@ -0,0 +1,28 @@
+// Private Properties with WeakMap in Javascript
+
+const _radius = new WeakMap()
+const _name = new WeakMap()
+const _resize = new WeakMap()
+
+class Circle{
+    constructor(radius, name){
+        this.size = 100
+        _radius.set = (this, radius)
+        _name.set = (this, name)
+        _resize.set(this, ()=>{
+            console.log(this.size);
+        })
+    }
+    draw(){
+        console.log('Drawing...');
+        console.log(_radius.get(this), _name.get(this));
+        _resize.get(this)()
+    }
+}
+
+let c1 = new Circle(2, 'Millat')
+
+c1.draw()
+
+//get symbol property
+
