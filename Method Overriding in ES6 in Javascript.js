795418679d8c7d0dd9ad8df8262b2472164cc49f
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-02-09 09:08:21 +0600

    Method Overriding in ES6 in Javascript

---
+++ Method Overriding in ES6 in Javascript.js
@@ -0,0 +1,33 @@
+//Method Overriding in ES6 in Javascript
+
+
+class Shape {
+    constructor(color){
+        this.color = color
+    }
+    draw(){
+        console.log('Drawing...');
+    }
+}
+
+class Rectangle extends Shape{
+    constructor(width, height){
+        super(this.color)
+        this.width = width
+        this.height = height
+    }
+
+    draw() {
+        console.log('Drawing Method Overriding');
+    }
+
+    calculate() {
+        return this.width * this.height
+    }
+}
+
+let rect = new Rectangle('green', 4, 5)
+
+console.log(rect);
+
+rect.draw()
